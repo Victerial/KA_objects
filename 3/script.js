315cbc36f624870da1ebdb0c2baddd0493531c55
Nándor <73957841+Victerial@users.noreply.github.com>
2023-10-14 12:34:05 +0200

    Add files via upload

---
+++ 3/script.js
@@ -0,0 +1,15 @@
+let fizetes={
+    Anna: 2100,
+    Cecil: 1890,
+    Emil: 2050,
+    Gerald: 2920
+}
+
+let ossz=0;
+for(let x in fizetes){
+    ossz+=fizetes[x]
+}
+
+console.log(ossz)
+
+document.write(ossz)
